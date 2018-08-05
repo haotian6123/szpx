9cf0748d0fced3b8f701bee4a1997c2a079733b4
haotian6123 <haotian6123@sina.com>
2018-08-05 12:15:10 +0800

    first commit

--- scripts/test_file.py
+++ scripts/test_file.py
@@ -2,10 +2,10 @@ class TestFile:
 
     def test_file1(self):
         assert 1
-
+    
     def test_file2(self):
         assert 1
-
+    
     def test_file3(self):
-        assert 1
+        assert 0
 
